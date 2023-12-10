ae5fd113f9903f6f8268f8961824d7596f53f604
Usama Rahman <95021960+usama-rahman@users.noreply.github.com>
2023-12-10 14:34:47 +0600

    👍

--- app.js
+++ app.js
@@ -1,24 +1,78 @@
-const pageOne = document.querySelector(".page1");
+function locoScroll() {
+  gsap.registerPlugin(ScrollTrigger);
 
-const cursor = document.querySelector(".cursor");
-
-pageOne.addEventListener("mousemove", function (dets) {
-  gsap.to(cursor, {
-    x: dets.clientX,
-    y: dets.clientY,
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector(".main"),
+    smooth: true,
   });
-});
+  locoScroll.on("scroll", ScrollTrigger.update);
 
-pageOne.addEventListener("mouseenter", function () {
-  gsap.to(cursor, {
-    scale: 1,
-    opacity: 1,
-  });
-});
+  ScrollTrigger.scrollerProxy(".main", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    },
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
 
-pageOne.addEventListener("mouseleave", function () {
-  gsap.to(cursor, {
-    scale: 0,
-    opacity: 0,
+    pinType: document.querySelector(".main").style.transform
+      ? "transform"
+      : "fixed",
   });
-});
+
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+  ScrollTrigger.refresh();
+}
+locoScroll();
+
+function page1Animation() {
+  const pageOne = document.querySelector(".page1");
+  const cursor = document.querySelector(".cursor");
+
+  pageOne.addEventListener("mousemove", function (dets) {
+    gsap.to(cursor, {
+      x: dets.clientX,
+      y: dets.clientY,
+    });
+  });
+
+  pageOne.addEventListener("mouseenter", function () {
+    gsap.to(cursor, {
+      scale: 1,
+      opacity: 1,
+    });
+  });
+
+  pageOne.addEventListener("mouseleave", function () {
+    gsap.to(cursor, {
+      scale: 0,
+      opacity: 0,
+    });
+  });
+}
+page1Animation();
+
+function page2Animation() {
+  gsap.from(".elem h1", {
+    y: 120,
+    stagger: 0.2,
+    duration: 1,
+    scrollTrigger: {
+      trigger: ".page2",
+      scroller: ".main",
+      start: "top 40%",
+      end: "top 37%",
+      // markers: true,
+      scrub: 2,
+    },
+  });
+}
+page2Animation();
